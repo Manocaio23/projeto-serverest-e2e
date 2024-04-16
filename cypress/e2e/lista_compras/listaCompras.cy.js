e2a83352a53465467613e78c7267e3451dedfe8f
Caio Augusto <caop18augusto@outlook.com>
2024-04-15 21:24:54 -0300

    concluido

--- cypress/e2e/lista_compras/listaCompras.cy.js
+++ cypress/e2e/lista_compras/listaCompras.cy.js
@@ -1,15 +1,7 @@
 
 describe('Teste de Login', () => {
     it('Deve fazer login como administrador', () => {
-      cy.visit('https://front.serverest.dev/login');
-      cy.get('#email').type('testenaoadm@outlook.com');
-      cy.get('#password').type('teste');
-      cy.get('[data-testid="entrar"]').click();
-      cy.wait(2000)
-    
-      //validação
-      cy.get('h1').should('contain.text', 'Serverest Store');
-
+      fazerLoginADM()
     });
   
    
