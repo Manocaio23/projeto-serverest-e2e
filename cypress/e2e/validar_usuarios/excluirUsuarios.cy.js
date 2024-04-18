7c66bfc1550633f4852869be99a480a3f5a19b4b
Caio Augusto <caop18augusto@outlook.com>
2024-04-17 21:01:15 -0300

    concluido

--- cypress/e2e/validar_usuarios/excluirUsuarios.cy.js
+++ cypress/e2e/validar_usuarios/excluirUsuarios.cy.js
@@ -1,9 +1,10 @@
-
-describe('Teste de Login', () => {
-    it('Deve fazer login como administrador', () => {
+import { faker } from '@faker-js/faker';
+describe('Deve clicar no botão de excluir e não existir o usuário', () => {
+    it('Validar que o usuário criado foi excluido', () => {
       const randomName = faker.person.fullName(); 
       const randomEmail = faker.internet.email();
 
+      cy.log(randomName); // mostra usuário
       cy.Criar_usuario(randomName,randomEmail);
       cy.fazerLoginADM();
       cy.get('[data-testid="listarUsuarios"]').click()
--- cypress/e2e/validar_usuarios/listarUsuarios.cy.js
+++ cypress/e2e/validar_usuarios/listarUsuarios.cy.js
@@ -1,15 +1,17 @@
-
-describe('Teste de Login', () => {
-    it('Deve fazer login como administrador', () => {
+import { faker } from '@faker-js/faker';
+describe('Validar que usuário está listado', () => {
+    
+  it('Deve validar usuário criado', () => {
       const randomName = faker.person.fullName(); 
       const randomEmail = faker.internet.email();
-
+      
+      cy.log(randomName); // mostra usuário
       cy.Criar_usuario(randomName,randomEmail);
       cy.fazerLoginADM();
       cy.get('[data-testid="listarUsuarios"]').click()
       cy.wait(2000)
       cy.get('td').should('be.visible').should('contain.text', randomName);
-      
+
 
     });
   
